9dc1c665a0c291b174e77a81be7181fccada64ab
be4ter <be4tter@gmail.com>
2018-08-23 17:19:29 +0900

    1

--- mrs2/src/main/java/mrs/app/room/RoomsController.java
+++ mrs2/src/main/java/mrs/app/room/RoomsController.java
@@ -3,9 +3,11 @@ package mrs.app.room;
 import mrs.domain.model.ReservableRoom;
 import mrs.domain.service.room.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,12 +20,38 @@ public class RoomsController {
     @Autowired
     RoomService roomService;
 
-    @GetMapping("/list")
+    @GetMapping("")
     String listRooms(Model model) {
         LocalDateTime today = LocalDateTime.now();
         List<ReservableRoom> rooms = roomService.findReservableRomms(today);
         model.addAttribute("date", today);
         model.addAttribute("rooms", rooms);
-        return "/room/listRooms";
+        return "room/listRooms";
+
+    }
+
+//    @GetMapping("/{date}")
+//    String listRooms(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDateTime localDateTime, Model model) {
+//        List<ReservableRoom> rooms = roomService.findReservableRomms(localDateTime);
+//        model.addAttribute("date", localDateTime);
+//        model.addAttribute("rooms", rooms);
+//        return "room/listRooms";
+//    }
+//    @RequestMapping(method = RequestMethod.GET)
+//    String listRooms(Model model) {
+//        LocalDateTime today = LocalDateTime.now();
+//        List<ReservableRoom> rooms = roomService.findReservableRomms(today);
+//        model.addAttribute("date", today);
+//        model.addAttribute("rooms", rooms);
+//        return "room/listRooms";
+//
+//    }
+
+    @RequestMapping(value = "{date}", method = RequestMethod.GET)
+    String listRooms(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDateTime localDateTime, Model model) {
+        List<ReservableRoom> rooms = roomService.findReservableRomms(localDateTime);
+        model.addAttribute("date", localDateTime);
+        model.addAttribute("rooms", rooms);
+        return "room/listRooms";
     }
 }
