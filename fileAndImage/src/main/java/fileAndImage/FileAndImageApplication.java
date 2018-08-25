3f224be78678d50d03375451e87e6a94abb52633
be4ter <be4tter@gmail.com>
2018-08-25 22:31:48 +0900

    init fileAndImage

---
+++ fileAndImage/src/main/java/fileAndImage/FileAndImageApplication.java
@@ -0,0 +1,12 @@
+package fileAndImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAndImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileAndImageApplication.class, args);
+	}
+}
